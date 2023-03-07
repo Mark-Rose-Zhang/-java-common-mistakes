a2290bc6f7021e6fbc6a995aefd084d45f47bbc4
zhangjinghao03 <zhangjinghao03@meituan.com>
2023-03-07 12:00:39 +0800

    init project

---
+++ java-common-mistakes/src/main/java/org/zhang/mistakes/StartApp.java
@@ -0,0 +1,16 @@
+package org.zhang.mistakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author NanCheng
+ * @version 1.0
+ * @date 2023/3/7 11:55
+ */
+@SpringBootApplication
+public class StartApp {
+    public static void main(String[] args) {
+        SpringApplication.run(StartApp.class, args);
+    }
+}
